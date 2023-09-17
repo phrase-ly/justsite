3092b23c03f08cc8c7de2639e18b9361a75c5313
jakubkaczmarski <kuba.kaczmarski312@gmail.com>
2023-09-17 13:50:47 +0200

    fix

--- app/projects/[slug]/header.tsx
+++ app/projects/[slug]/header.tsx
@@ -46,7 +46,7 @@ export const Header: React.FC<Props> = ({ project, views }) => {
 			className="relative isolate overflow-hidden bg-gradient-to-tl from-black via-zinc-900 to-black"
 		>
 			<div
-				className={`fixed inset-x-0 top-0 z-50 backdrop-blur lg:backdrop-blur-none duration-200 border-b lg:bg-transparent ${
+				className={`fixed inset-x-0 top-0 z-50 backdrop-blur lg:backdrop-blur-none duration-200 border-b  ${
 					isIntersecting
 						? "bg-zinc-900/0 border-transparent"
 						: "bg-white/10  border-zinc-200 lg:border-transparent"

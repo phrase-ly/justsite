d702a5e08c1f640f3564126477f142e6f84a378a
jakubkaczmarski <kuba.kaczmarski312@gmail.com>
2023-09-17 13:41:23 +0200

    instructions link added

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,8 @@ export default function Home() {
 					phrase.ly is an amazing plugin that unleashes the power of productivity
 					{" "}
 				</h2>
-				<p><a className="text-sm text-zinc-500 underline text-brand" href="https://github.com/phrase-ly/phrasely/releases/download/0.0.1/phrasely.zip">Download</a></p>
+				<p><a className="text-sm text-zinc-500 underline text-brand" href="https://github.com/phrase-ly/phrasely/releases/download/0.0.1/phrasely.zip">Download</a> {"             "}
+				<a className="text-sm text-zinc-500 underline text-brand" href="https://github.com/phrase-ly/phrasely">Instructions</a></p>
 			</div>
 		</div>
 	);
